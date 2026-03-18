4fa2cac7763f402f331b01601791ad666f9fbe4b
GabrielSchaller <S4schaller@gmail.com>
2026-03-18 20:40:28 +0100

    jsonprep

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-import fs from 'fs';
-
 function scroll(e){
     e.style.transform = "translateY(10000px)";
 }
@@ -18,13 +16,44 @@ var button = document.getElementById("enter");
 button.addEventListener("click", function(e){
     e.target.style.background = "pink";
 });
+function shuffle(array){
+    var currentIndex = array.length;
+    while(currentIndex != 0){
+        var rand = Math.floor(Math.random()*currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[rand]] = [array[rand], array[currentIndex]];
+    }
+}
 
 function init_imgs(){
     console.log("init");
-    var dir = "bilder";
-    var files = fs.readdirSync(dir);
+    var files = [];
+    fetch("files.txt")
+    .then((res) => res.text())
+    .then((text) => {
+        var json = JSON.parse(text);
+        console.log(json);
+        files.append();
+    })
+    .catch((e) => console.error(e));
+    shuffle(files);
+    var columns = Array.from(document.getElementsByClassName("column"));
+    var firstColumn =  columns[0];
+    var secondColumn = document.getElementsByClassName("columnR")[0];
+    var thirdColumn = columns[1];
     for(var i=0; i<files.length;i++){
-        console.log(i);
+        const img = document.createElement("img");
+        img.classList.add("picture");
+        const cont = document.createElement("div");
+        cont.classList.add("container");
+        cont.append(img);
+        img.src = files[i];
+        if(i%3 == 0){
+            firstColumn.appendChild(cont)
+        }else if(i%3 == 1){
+            secondColumn.appendChild(cont)
+        }else{
+            thirdColumn.appendChild(cont)
+        }
     }
-    files = []
 }
